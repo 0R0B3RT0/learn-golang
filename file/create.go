bd48d43e2c58c957f0010b0d246ef8be9d70bf1e
Roberto Candido de Oliveira Júnior <roberto.candido@contaazul.com>
2020-07-24 00:18:59 -0300

    refact: add function main

--- file/create.go
+++ file/create.go
@@ -1,8 +1,14 @@
-package file
+package main
 
 import (
+	"fmt"
 	"os"
 )
+
+func main() {
+	input := os.Args[1:]
+	fmt.Printf("%v\n", CreateFile(input[0], input[1:]...))
+}
 
 func CreateFile(baseDir string, names ...string) []string {
 	var files = []string{}
