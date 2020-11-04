9a2fb69089f01d62578ed4bb81a597277a2a94c7
Roberto Candido de Oliveira Júnior <roberto.candido@contaazul.com>
2020-11-03 23:04:03 -0300

    doc: Example of named loop and switch

--- interators/loog-with-name.go
+++ interators/loog-with-name.go
@@ -10,9 +10,25 @@ externo:
 	for {
 		for i = 0; i < 10; i++ {
 			if i == 5 {
+				fmt.Println("Quebrando for externo.", i)
 				break externo
 			}
 		}
 	}
 	fmt.Println(i)
+
+loop:
+	for i = 0; i < 10; i++ {
+		fmt.Printf("for i = %d\n", i)
+
+		switch i {
+		case 2, 3:
+			fmt.Printf("Quebrando switch, i == %d.\n", i)
+			break
+		case 5:
+			fmt.Println("Quebrando loop, i == 5.")
+			break loop
+		}
+	}
+	fmt.Println("Fim.")
 }
