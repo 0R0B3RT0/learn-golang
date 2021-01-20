fbba2a9093b099b4980f04d8103a3188d4d67b19
Roberto Oliveira <roberto.oliveira@pismo.io>
2021-01-20 14:31:13 -0300

    exemplo http

---
+++ 22-HTML/.docker/init.sql
@@ -0,0 +1,5 @@
+create table usuarios(
+    id int auto_increment primary key,
+    nome varchar(50) not null,
+    email varchar(50) not null
+);
---
+++ 22-HTML/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+type usuario struct {
+	Nome  string
+	Email string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	u := usuario{"João", "joao@email.com"}
+	templates.ExecuteTemplate(w, "home.html", u)
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob(("*.html")))
+
+	http.HandleFunc("/home", home)
+
+	fmt.Println("Escutando a porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
